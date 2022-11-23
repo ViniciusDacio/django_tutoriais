1f1e01470d4caf07c12701719a9bab3d91c8340e
Vinicius Dacio <viniciusdacio@gmail.com>
2022-11-23 15:09:55 -0300

    Django Vue

---
+++ Tutorial_REST_09/client/pages/recipes/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <main class="container mt-5">
+    <div class="row">
+      <div class="col-12 text-right mb-4">
+        <div class="d-flex justify-content-between">
+          <h3>La Recipes</h3>
+
+          <nuxt-link to="/recipes/add"
+class="btn btn-info"
+            >Add Recipe</nuxt-link
+          >
+        </div>
+      </div>
+      <template v-for="recipe in recipes">
+        <div :key="recipe.id" class="col-lg-3 col-md-4 col-sm-6 mb-4">
+          <recipe-card :onDelete="deleteRecipe" :recipe="recipe"></recipe-card>
+        </div>
+      </template>
+    </div>
+  </main>
+</template>
+<script>
+import RecipeCard from '~/components/RecipeCard.vue'
+
+export default {
+  async asyncData ({ $axios, params }) {
+    try {
+      const recipes = await $axios.$get('/recipes/')
+      return { recipes }
+    } catch (e) {
+      return { recipes: [] }
+    }
+  },
+  head () {
+    return {
+      title: 'Recipes list'
+    }
+  },
+  components: {
+    RecipeCard
+  },
+  data () {
+    return {
+      recipes: []
+    }
+  },
+  methods: {
+    // eslint-disable-next-line camelcase
+    async deleteRecipe (recipe_id) {
+      try {
+        // eslint-disable-next-line camelcase
+        await this.$axios.$delete(`/recipes/${recipe_id}/`) // delete recipe
+        const newRecipes = await this.$axios.$get('/recipes/') // get new recipes
+        this.recipes = newRecipes // update list of recipes
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
